ea97500dd7481fb958d5c13191e93e56757f290a
vishwajeethaldar <onlyjeet3@gmail.com>
2022-09-15 03:02:39 +0530

    skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -6,13 +6,13 @@ export const Skills = () => {
   return (
     
     <Box w="100%">
-        <Box py="50px" textAlign={"center"} as='h1' fontSize={["1.2em","1.4em","1.8em"]} fontWeight={"700"}>
+        <Box py="50px" textAlign={"center"} as='h1' fontSize={["1.8em","1.8em","1.8em"]} fontWeight={"700"}>
            {` Skills & Interests`}
         </Box>
        <Flex justify={"center"} mx={"auto"} gap={["15xp", "15px", "20px", "20px"]} w={["96%", "96%", "80%", "80%"]} flexWrap="wrap">
         {skillsList.map((skill)=>{
             return (
-              <Box key={skill.id} w={["200px"]} boxShadow={"xl"}>
+              <Box key={skill.id} w={["40%","40%","20%","18%"]} boxShadow={"xl"} mx="1%" my="2%">
                 <Sikll image={skill.image} title={skill.title} url={skill.url} />
               </Box>
             )
